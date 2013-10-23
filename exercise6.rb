02d21498457bfc1396a96a8e5d74598ad0205452
AM-lvl3 <drewmorris@live.com>
2013-10-23 17:49:02 -0400

    updated with rice

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,13 @@
+def print_list(list)
+	list.each do |groceries|
+		puts "* " + groceries
+	end
+end
+
 grocery_list = ["almonds", "salad", "hot sauce", "beef", "beer"]
 
-grocery_list.each do |groceries|
-puts "* " + groceries
-end
+print_list(grocery_list)
+
+grocery_list << "rice"
+
+print_list(grocery_list)
